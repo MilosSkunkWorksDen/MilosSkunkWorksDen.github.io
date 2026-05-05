445df2965cc2762c707f0793dd5ab184d0a46bde
Milos Cabrkapa <milos.cabarkapa@skunkworksden.com>
2026-05-05 09:46:37 +0000

    NewProject Confirm Dialog

---
+++ src/components/new-project/ConfirmDialog.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog'
+
+defineEmits(['confirmed'])
+</script>
+
+<template>
+  <AlertDialog>
+    <AlertDialogTrigger> <slot /></AlertDialogTrigger>
+    <AlertDialogContent>
+      <AlertDialogHeader>
+        <AlertDialogTitle class="">Start New Project ?</AlertDialogTitle>
+        <AlertDialogDescription>
+          This action cannot be undone. This will permanently delete your data and start a clean
+          project.
+        </AlertDialogDescription>
+      </AlertDialogHeader>
+      <AlertDialogFooter>
+        <AlertDialogCancel>Cancel</AlertDialogCancel>
+        <AlertDialogAction
+          class="bg-red-600 text-white hover:bg-red-700 focus:ring-red-500"
+          @click="$emit('confirmed')"
+          >Confirm</AlertDialogAction
+        >
+      </AlertDialogFooter>
+    </AlertDialogContent>
+  </AlertDialog>
+</template>
--- src/components/new-project/NewProject.vue
+++ src/components/new-project/NewProject.vue
@@ -4,31 +4,32 @@ import Link, { type Props as LinkProps } from './Link.vue'
 import type { LucideIcon } from '@lucide/vue'
 import { computed, type HTMLAttributes, type HtmlHTMLAttributes } from 'vue'
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip'
-import Button from './ui/button/Button.vue'
 import { useRoute } from 'vue-router'
 import type { PrimitiveProps } from 'reka-ui'
 import type { ButtonVariants } from '../ui/button'
+import ConfirmDialog from './ConfirmDialog.vue'
+import Button from '../ui/button/Button.vue'
 
 function flush() {
-  if (confirm('All data will be lost')) {
-    localStorage.removeItem('tables')
-    localStorage.removeItem('db_people')
-    window.location.reload()
-  }
+  localStorage.removeItem('tables')
+  localStorage.removeItem('db_people')
+  window.location.reload()
 }
 </script>
 
 <template>
-  <TooltipProvider>
-    <Tooltip>
-      <TooltipTrigger as-child>
-        <Button @click="flush" variant="ghost" class="size-6!" size="icon-sm">
-          <Plus class="size-4!"></Plus>
-        </Button>
-      </TooltipTrigger>
-      <TooltipContent side="right" align="center">
-        <p>{{ 'Start New Project' }}</p>
-      </TooltipContent>
-    </Tooltip>
-  </TooltipProvider>
+  <ConfirmDialog @confirmed="flush">
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger as-child>
+          <Button variant="ghost" class="size-6!" size="icon-sm">
+            <Plus class="size-4!"></Plus>
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent side="right" align="center">
+          <p>{{ 'New Project' }}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  </ConfirmDialog>
 </template>
